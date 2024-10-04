20409ed71961d03df8f69c4e1627e0e5efc47ccf
Krish Tambe <krish.tambe@gmail.com>
2024-10-04 13:08:55 +0530

    Data validation completed

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_02_data_validation import DataValdationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion Stage"
+
 
 try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
@@ -10,4 +12,15 @@ try:
     logger.info(f">>>>> stage {STAGE_NAME} started <<<<<\n\nx===============x")
 except Exception as e:
     logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Validation Stage"
+
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    obj = DataValdationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx============x")
+except Exception as e:
     raise e
